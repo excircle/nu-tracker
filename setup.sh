589f85f1449b47b3d5fd524a2099abbcd1bbe917
Alexander Kalaj <akalaj@Alexanders-MacBook-Air.local>
2023-08-03 07:51:56 -0700

    Adding gitignore, setup.sh, and base docker-compose files

---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir ./frontend-data
+mkdir ./mysql-data
+mkdir ./mysql-config
+mkdir ./api-data
+echo './frontend-data' >> ./.gitignore
+echo './mysql-data' >> ./.gitignore
+echo './mysql-config' >> ./.gitignore
+echo './api-data' >> ./.gitignore
